6fcc3a2b5193b0b848e1782e87f62e6a9412fe1b
unknown <2155609@labinfo.is.escuelaing.edu.co>
2019-08-22 10:06:21 -0500

    Pruebas

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -27,7 +27,7 @@ public class RegistryTest {
 	
 	@Test
     public void shouldBeUndarage() {
-        Person person = new Person("Alan Brito", 1132492643, 17, Gender.MALE, true);
+        Person person = new Person("Alan Brito", 1132492643, 10, Gender.MALE, true);
         RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
@@ -54,7 +54,49 @@ public class RegistryTest {
 		result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.DUPLICATED, result);
     }
-
+	
+	@Test
+    public void shouldBeInvalidAge1Negative() {
+        Person person = new Person("Pruebastian", 22304764, -1, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+	
+	@Test
+    public void shouldBeUndarageAge0() {
+        Person person = new Person("Pruebastian2", 22375476, 0, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+	
+	@Test
+	public void shouldBeUndarageAge1() {
+        Person person = new Person("Pruebastian3", 26790476, 1, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+	
+	@Test
+	public void shouldBeUndarageAge17() {
+        Person person = new Person("Pruebastina1", 46680476, 17, Gender.FEMALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+	
+	@Test
+	public void shouldBeValidAge18() {
+        Person person = new Person("Pruebastina2", 46683427, 18, Gender.FEMALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+	
+	@Test
+	public void shouldBeValidAge19() {
+        Person person = new Person("Pruebastina3", 42663787, 19, Gender.FEMALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+	
 
     // TODO Complete with more test cases
 }
